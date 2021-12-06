939e384563f287acceeb3a03e9ee3e80f6a69fad
BuildTools <unconfigured@null.spigotmc.org>
2021-12-06 13:42:21 -0700

    assuming err means error I added some logs.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -7,8 +7,11 @@ var mongoose = require('mongoose'),
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
   new_task.save(function(err, task) {
-    if (err)
+    if (err){
+      console.log('Error in create_a_task: ' + err);
       res.send(err);
+    }
+    console.log('Creating ' + task);
     res.json(task);
   });
 };
@@ -16,8 +19,11 @@ exports.create_a_task = function(req, res) {
 
 exports.read_a_task = function(req, res) {
   Task.findById(req.params.taskId, function(err, task) {
-    if (err)
+    if (err){
+      console.log('Error in read_a_task: ' + err);
       res.send(err);
+    }
+    console.log('Reading ' + task);
     res.json(task);
   });
 };
@@ -26,8 +32,11 @@ exports.read_a_task = function(req, res) {
 exports.create_a_user = function(req, res) {
   var new_user = new User(req.body);
   new_user.save(function(err, user) {
-    if (err)
+    if (err){
+      console.log('Error in create_a_user: ' + err);
       res.send(err);
+    }
+    console.log('Created user ' + user);
     res.json(user);
   });
 };
