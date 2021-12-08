7c0b55f0134bc780f9b7c254c5b99c322cd8b488
BuildTools <unconfigured@null.spigotmc.org>
2021-12-08 13:05:26 -0700

    Updated route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,13 +1,17 @@
 'use strict';
+const auth = require("./middleware/auth");
 module.exports = function(app) {
   var user = require('../controllers/userController');
 
   // todoList Routes
   app.route('/createuser')
     .post(user.create_a_user);
-
+    
+  app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+  });
 
   app.route('/login')
-    .post(user.login)
+    .post(user.login);
 
 };
